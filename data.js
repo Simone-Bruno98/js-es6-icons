6972f65bd95dcb6e4433d5b37d453851cc0f3273
Simone-Bruno98 <bruno-simone@outlook.it>
2022-01-13 23:29:15 +0100

    Milestone 3 finito con un piccolo errore

--- data.js
+++ data.js
@@ -1,4 +1,4 @@
-[
+let iconsArr = [
 	{
 		name: 'cat',
 		prefix: 'fa-',
@@ -112,3 +112,53 @@
 		color: 'blue'
 	}
 ];
+
+let container = document.querySelector(".row");
+let type = document.getElementById("type");
+const animalArr = iconsArr.filter((element) => element.type == "animal");
+const vegetableArr = iconsArr.filter((element) => element.type == "vegetable");
+const userArr = iconsArr.filter((element) => element.type == "user");
+//
+// for(let i = 0; i < iconsArr.length; i++){
+//     document.querySelector(".row").innerHTML += `
+//     	<div style="color:${iconsArr[i].color}" class="col-2 mx-3 my-3 card text-center">
+// 			<i class="${iconsArr[i].family} ${iconsArr[i].prefix}${iconsArr[i].name}"></i>
+// 			<h3 class="cardname">${iconsArr[i].name}</h3>
+//     	</div>
+// 	`;
+//   }
+
+
+function createCard(array) {
+array.forEach((icon, i) => {
+	container.innerHTML += ` 
+	<div style="color:${iconsArr[i].color} " class="${iconsArr[i].family} col-2 mx-3 my-3 card text-center ">
+		<i class="${array[i].type} ${iconsArr[i].family} ${iconsArr[i].prefix}${iconsArr[i].name}"></i>
+		<h3 class="cardname">${iconsArr[i].name}</h3>
+	</div>
+	`;
+});
+}
+
+createCard(iconsArr);
+
+type.addEventListener("change",function(){
+	switch (this.value){
+		case "all":
+			container.innerHTML = "";
+			createCard(iconsArr);
+			break;
+		case "animal":
+			container.innerHTML = "";
+			createCard(animalArr);
+			break;
+		case "vegetable":
+			container.innerHTML = "";
+			createCard(vegetableArr);
+			break;
+		case "user":
+			container.innerHTML = "";
+			createCard(userArr);
+			break;
+	}
+});
